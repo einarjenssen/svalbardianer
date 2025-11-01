bb1c893814b0efedcb644ec6318bb1bb0885de1f
Einar Jenssen <ejenssen80@gmail.com>
2025-11-01 23:56:22 +0100

    Mer småtteri

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,11 @@
 import { createAuthClient } from "better-auth/svelte";
+import { InferAuth } from "better-auth/client";
 import { passkeyClient } from "better-auth/client/plugins";
 
+type AppAuth = typeof import("$lib/auth").auth;
+
 export const authClient = createAuthClient({
+  ...InferAuth<AppAuth>(),
   plugins: [
     passkeyClient()
   ]
